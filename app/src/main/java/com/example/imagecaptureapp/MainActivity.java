71d070ab27f64b895734ef03667d3161a9592828
prashant <csprshantphuse@gmail.com>
2021-07-30 16:53:17 +0530

    added with functionality

--- app/src/main/java/com/example/imagecaptureapp/MainActivity.java
+++ app/src/main/java/com/example/imagecaptureapp/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.imagecaptureapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
-
+private static final int REQUEST_CAPTURE_IMAGE=1;
+ImageView image;
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image=findViewById(R.id.imagev);
+        btn=findViewById(R.id.captuerBtn);
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},100);
+        }
+    btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent,REQUEST_CAPTURE_IMAGE);
+        }});
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CAPTURE_IMAGE) {
+             switch (resultCode) {
+                 case Activity.RESULT_OK:
+                     Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+                     image.setImageBitmap(captureImage);
+                     break;
+                 case Activity.RESULT_CANCELED:
+                     break;
+                 default:
+                     break;
+             }
+        }
+    }
+
 }
